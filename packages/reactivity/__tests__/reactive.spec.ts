920613ffcc13831c97e01c0f4d3adc0ed34087ff
“zhuenguang” <764956153@qq.com>
2022-07-03 10:52:09 +0800

    实现isreadonly和isreactive

--- packages/reactivity/__tests__/reactive.spec.ts
+++ packages/reactivity/__tests__/reactive.spec.ts
@@ -1,9 +1,13 @@
 import { reactive } from '../src/index';
+import { isReactive } from '../src/reactive';
 describe('effect', () => {
     it('happy path', () => {
        const original = {foo: 1};
        const observed = reactive(original);
        expect(observed).not.toBe(original);
        expect(observed.foo).toBe(1);
+       expect(isReactive(observed)).toBe(true);
+       expect(isReactive(original)).toBe(false);
+       expect(isReactive(original)).toBe(false);
 ;    })
 })
--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,12 +1,17 @@
 import { track, trigger } from "./effect";
+import { ReactiveFlags } from "./reactive";
 
 const get = createGetter();
 const set = createSetter();
 const readonlyGet = createGetter(true)
 function createGetter(isReadonly = false) {
     return function get(target, key) {
+        if(key === ReactiveFlags.IS_REACTIVE) {
+            return !isReadonly;
+        }else if(key === ReactiveFlags.IS_READONLY) {
+            return isReadonly;
+        }
         const res = Reflect.get(target, key);
-        //  todo 依赖收集
         if(!isReadonly) {
             track(target, key);
         }
